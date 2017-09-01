e4b24613af040c5d2d7445c554acb95c9e6d7b4f
darealshinji <djcj@gmx.de>
2017-09-01 07:14:38 +0200

    svg to png: enable convertion from string

--- svg_to_png_librsvg.c
+++ svg_to_png_librsvg.c
@@ -19,7 +19,9 @@ typedef char gchar;
 typedef int gint;
 typedef gint gboolean;
 typedef unsigned int guint;
+typedef unsigned char guint8;
 typedef unsigned int guint32;
+typedef unsigned long gsize;
 typedef guint GType;
 typedef guint32 GQuark;
 typedef struct _GObject GObject;
@@ -96,6 +98,7 @@ struct _RsvgDimensionData { int width; int height; gdouble em; gdouble ex; };
 #define LOAD_SYMBOL(a,b,c,d)
 #define DLCLOSE_ALL
 #define dl_rsvg_handle_new_from_stream_sync rsvg_handle_new_from_stream_sync
+#define dl_rsvg_handle_new_from_data rsvg_handle_new_from_data
 #define dl_rsvg_handle_get_dimensions rsvg_handle_get_dimensions
 #define dl_rsvg_handle_render_cairo rsvg_handle_render_cairo
 #define dl_rsvg_cleanup rsvg_cleanup
@@ -110,22 +113,17 @@ struct _RsvgDimensionData { int width; int height; gdouble em; gdouble ex; };
 
 #endif  // USE_DLOPEN
 
-int pipefd[2];
 char *svg_to_png_librsvg_buffer = NULL;
 ssize_t svg_to_png_librsvg_buffer_length = 0;
 
-#define SVG_TO_PNG_LIBRSVG_COPY(buffer, length) \
+#define svg_to_png_librsvg_copy(buffer, length) \
   buffer = (char *)malloc(svg_to_png_librsvg_buffer_length); \
   length = svg_to_png_librsvg_buffer_length; \
   memcpy(buffer, svg_to_png_librsvg_buffer, length);
 
-void svg_to_png_librsvg_free(void *buffer)
-{
-  if (buffer)
-  {
-    free(buffer);
-  }
-}
+#define svg_to_png_librsvg_free(x)       if (x) { free(x); }
+#define svg_to_png_librsvg_from_file(x)  svg_to_png_librsvg(x, NULL)
+#define svg_to_png_librsvg_from_data(x)  svg_to_png_librsvg(NULL, x)
 
 void svg_to_png_librsvg_free_global(void)
 {
@@ -137,8 +135,10 @@ void svg_to_png_librsvg_free_global(void)
   svg_to_png_librsvg_buffer_length = 0;
 }
 
-static cairo_status_t rsvg_cairo_write_func(void *closure, const unsigned char *data, unsigned int length)
+static inline
+cairo_status_t rsvg_cairo_write_func(void *closure, const unsigned char *data, unsigned int length)
 {
+  int *pipefd = (int *)closure;
   if (write(pipefd[1], data, length) == length)
   {
     return 0;
@@ -146,7 +146,7 @@ static cairo_status_t rsvg_cairo_write_func(void *closure, const unsigned char *
   return 1;
 }
 
-int svg_to_png_librsvg(const char *input)
+int svg_to_png_librsvg(const char *input_file, const char *input_data)
 {
   INIT_DLOPEN
 
@@ -155,6 +155,7 @@ int svg_to_png_librsvg(const char *input)
   LOAD_LIBRARY( h_gio,   "libgio-2.0.so.0",  dlclose(h_rsvg); dlclose(h_cairo) )
   LOAD_LIBRARY( h_glib,  "libglib-2.0.so.0", dlclose(h_rsvg); dlclose(h_cairo); dlclose(h_glib) )
 
+  LOAD_SYMBOL( h_rsvg,  RsvgHandle *,       rsvg_handle_new_from_data,         (const guint8 *, gsize, GError **) )
   LOAD_SYMBOL( h_rsvg,  RsvgHandle *,       rsvg_handle_new_from_stream_sync,  (GInputStream *, GFile *, cairo_status_t, GCancellable *, GError **) )
   LOAD_SYMBOL( h_rsvg,  void,               rsvg_handle_get_dimensions,        (RsvgHandle *, RsvgDimensionData *) )
   LOAD_SYMBOL( h_rsvg,  gboolean,           rsvg_handle_render_cairo,          (RsvgHandle *, cairo_t *) )
@@ -176,23 +177,31 @@ int svg_to_png_librsvg(const char *input)
   cairo_surface_t *surface;
   cairo_t *cr;
   cairo_status_t save_status = 1;
+  int pipefd[2];
   char buf;
   char *png_buf = NULL;
   ssize_t png_buf_length = 0;
   ssize_t length = 0;
   int rv = 1;
 
-  file = dl_g_file_new_for_path(input);
-  stream = (GInputStream *)dl_g_file_read(file, NULL, &error);
-  if (error)
+  if (input_data)
   {
-    fprintf(stderr, "%s: `%s'\n", error->message, input);
-    dl_g_error_free(error);
-    DLCLOSE_ALL
-    return 1;
+    rsvg = dl_rsvg_handle_new_from_data((guint8 *)input_data, (gsize) strlen(input_data), &error);
+  }
+  else
+  {
+    file = dl_g_file_new_for_path(input_file);
+    stream = (GInputStream *)dl_g_file_read(file, NULL, &error);
+    if (error)
+    {
+      fprintf(stderr, "%s: `%s'\n", error->message, input_file);
+      dl_g_error_free(error);
+      DLCLOSE_ALL
+      return 1;
+    }
+    rsvg = dl_rsvg_handle_new_from_stream_sync(stream, file, 0, NULL, &error);
   }
 
-  rsvg = dl_rsvg_handle_new_from_stream_sync(stream, file, 0, NULL, &error);
   if (error)
   {
     fprintf(stderr, "%s\n", error->message);
@@ -225,7 +234,7 @@ int svg_to_png_librsvg(const char *input)
   {
     /* Child process */
     close(pipefd[0]);
-    save_status = dl_cairo_surface_write_to_png_stream(surface, rsvg_cairo_write_func, NULL);
+    save_status = dl_cairo_surface_write_to_png_stream(surface, rsvg_cairo_write_func, pipefd);
     close(pipefd[1]);
     _exit(save_status);
   }
@@ -272,10 +281,39 @@ int main(void)
   char *buf = NULL;
   ssize_t len = 0;
 
-  rv = svg_to_png_librsvg("fltk/Applications-multimedia.svg");
+  const char *svg_data = /* Applications-other.svg */
+  "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='48' height='48'><defs><linearG"
+  "radient id='c'><stop offset='0' stop-color='#fff'/><stop offset='1' stop-color='#fff' stop-opacity='0'/></linearGradient"
+  "><linearGradient id='b'><stop offset='0' stop-color='#fcaf3e'/><stop offset='1' stop-color='#d37f03'/></linearGradient><"
+  "linearGradient id='a'><stop offset='0' stop-opacity='.327'/><stop offset='1' stop-opacity='0'/></linearGradient><radialG"
+  "radient gradientUnits='userSpaceOnUse' r='11.69' fy='72.568' fx='14.288' cy='68.873' cx='14.288' gradientTransform='matr"
+  "ix(1.4 0 0 .513 4.365 4.84)' id='d' xlink:href='#a'/><radialGradient xlink:href='#b' id='e' cx='24.286' cy='36.721' fx='"
+  "24.286' fy='36.721' r='20.411' gradientUnits='userSpaceOnUse' gradientTransform='translate(-17.677 -26.728) scale(1.728)"
+  "'/><linearGradient xlink:href='#c' id='f' x1='26.503' y1='10.027' x2='28.786' y2='52.807' gradientUnits='userSpaceOnUse'"
+  "/></defs><sodipodi:namedview pagecolor='#ffffff' bordercolor='#666666' borderopacity='1' showgrid='false'/><path transfo"
+  "rm='translate(-4.54 -7.795) scale(1.186)' d='M44.286 38.714a19.93 9.837 0 1 1-39.857 0 19.93 9.837 0 1 1 39.85 0z' sodip"
+  "odi:ry='9.837' sodipodi:rx='19.929' sodipodi:cy='38.714' sodipodi:cx='24.357' sodipodi:type='arc' color='#000' fill='url"
+  "(#d)' fill-rule='evenodd' overflow='visible'/><path d='M24.286 43.196l-19.91-19.91 19.91-19.91 19.91 19.91-19.91 19.91z'"
+  " fill='url(#e)' stroke='#f57900' stroke-linecap='round' stroke-linejoin='round'/><path d='M39.44 19.377L38.7 20.4c-.635-"
+  ".268-1.3-.484-1.985-.644l.006-1.577c-.42-.09-.86-.16-1.3-.21l-.48 1.5c-.34-.03-.69-.05-1.04-.05-.35 0-.7.01-1.04.04l-.48"
+  "-1.5c-.44.05-.87.11-1.3.2l.01 1.574c-.69.16-1.35.377-1.99.646l-.92-1.28c-.402.18-.794.38-1.175.6l.493 1.5c-.596.36-1.16."
+  "77-1.686 1.226l-1.275-.934c-.324.3-.636.61-.932.937l.93 1.27c-.452.526-.864 1.09-1.226 1.69l-1.5-.495c-.22.38-.42.774-.6"
+  " 1.176l1.28.92c-.27.637-.49 1.3-.65 1.986l-1.578-.003c-.09.425-.16.86-.207 1.3l1.505.48c-.03.345-.046.69-.046 1.045 0 .3"
+  "5.018.7.047 1.04l-1.503.483c.048.44.116.874.204 1.3l1.578-.002c.16.687.377 1.35.645 1.987l-1.28.92c.18.403.38.795.6 1.17"
+  "6l1.5-.493c.36.595.772 1.16 1.227 1.685l-.93 1.272c.28.307.57.6.876.88L43.3 23.23l-1.008-1.005-.3.218c-.525-.455-1.09-.8"
+  "66-1.686-1.228l.242-.735-1.108-1.11v.002zM19.917 14.33c0 6.485-4.234 11.98-10.088 13.875l2.22 2.227.05-.02.18.252 1.41 1"
+  ".412c.46-.22.91-.455 1.36-.708l-.7-2.11c.84-.51 1.63-1.088 2.37-1.728l1.79 1.312c.46-.418.896-.856 1.313-1.315l-1.31-1.7"
+  "9c.64-.74 1.22-1.535 1.73-2.377l2.11.695c.305-.537.59-1.09.844-1.655l-1.8-1.298c.38-.897.68-1.83.905-2.797l2.22.01c.126-"
+  ".604.22-1.217.29-1.837l-2.116-.68c.04-.483.063-.973.063-1.467 0-.49-.02-.98-.063-1.46l2.118-.68c-.07-.62-.17-1.23-.29-1."
+  "83l-2.22.01c-.23-.96-.53-1.9-.91-2.79l1.8-1.3c-.14-.31-.295-.62-.45-.93l-3.8 3.806c.614 1.615.952 3.366.952 5.197z' opac"
+  "ity='.5' fill='#fff'/><path d='M24.286 41.605l-18.32-18.32 18.32-18.32 18.32 18.32-18.32 18.32z' fill='none' stroke='url"
+  "(#f)' opacity='.473'/></svg>";
+
+  //rv = svg_to_png_librsvg_from_file("fltk/Applications-multimedia.svg");
+  rv = svg_to_png_librsvg_from_data(svg_data);
   if (rv == 0)
   {
-    SVG_TO_PNG_LIBRSVG_COPY(buf, len);
+    svg_to_png_librsvg_copy(buf, len);
     fwrite(buf, len, 1, stdout);
     fflush(stdout);
   }
