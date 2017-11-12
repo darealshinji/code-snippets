88ace647a06af553f9b868de56efa43964fd6000
darealshinji <djcj@gmx.de>
2017-11-12 17:09:14 +0100

    Rewrite getSelfExe.c

--- getSelfExe.c
+++ getSelfExe.c
@@ -1,85 +1,29 @@
-// retrieve the full path of the current executable
-
-#include <errno.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <errno.h>
 
-/* testing */
-//#undef PATH_MAX
-//#define PATH_MAX 9
-
-//#define ALLOCATE_BUFFER
-
-#ifndef ALLOCATE_BUFFER
-static char _getSelfExeBuf[PATH_MAX + 1];
-#endif
-
-char *getSelfExe()
-{
-  char *buf;
-  ssize_t size;
-
-#ifdef ALLOCATE_BUFFER
-  buf = malloc(PATH_MAX + 1);
-#else
-  buf = _getSelfExeBuf;
-#endif
-  memset(buf, '\0', PATH_MAX + 1);
-  size = readlink("/proc/self/exe", buf, PATH_MAX + 1);
-
-  if (size == -1)
-  {
-#ifdef ALLOCATE_BUFFER
-    free(buf);
-#endif
-    return NULL;
-  }
-
-  if (buf[PATH_MAX] != '\0')
-  {
-    errno = ENAMETOOLONG;
-    buf[PATH_MAX] = '\0';
-  }
-
-  return buf;
-}
+/* retrieve the full path of the current executable */
 
 int main(void)
 {
-  char *self = getSelfExe();
-  int errno_returned = errno;
+  const char *self = "/proc/self/exe";
+  char path[PATH_MAX + 1] = {0};
 
-  if (self == NULL)
+  char *rp = realpath(self, path);
+  int errsv = errno;
+
+  if (rp)
   {
+    printf("%s\n", path);
+  }
+  else
+  {
+    fprintf(stderr, "%s(): %s\n", __func__, strerror(errsv));
     return 1;
   }
 
-  if (errno_returned == ENAMETOOLONG)
-  {
-    fprintf(stderr, "warning: path length exceeds %d bytes and was truncated!\n", PATH_MAX);
-  }
-  printf("path: %s\n", self);
-  printf("length: %ld\n", strlen(self));
-
-#ifdef ALLOCATE_BUFFER
-  free(self);
-#endif
   return 0;
 }
-
-/* a more simple example */
-/*
-int main(void)
-{
-  char buf[PATH_MAX + 1] = {0};
-  if (readlink("/proc/self/exe", buf, PATH_MAX) == -1) {
-    return 1;
-  }
-  printf("%s\n", buf);
-  return 0;
-}
-*/
 
