a0c5f9af44bd99d54c4334570bb421d604a2c4fb
darealshinji <djcj@gmx.de>
2018-01-29 06:43:16 +0100

    strlastcasecmp()

--- strlastcmp.c
+++ strlastcmp.c
@@ -1,8 +1,11 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
 size_t strlastcmp(const char *s1, const char *s2);
+size_t strlastcasecmp(const char *s1, const char *s2);
 int str_ends_on(const char *s, const char *suf);
+int strcase_ends_on(const char *s, const char *suf);
 
 /* Compares the last bytes of s1 and s2 and returns the number of equal bytes.
  * The terminating null byte ('\0') is ignored.
@@ -14,30 +17,50 @@ size_t strlastcmp(const char *s1, const char *s2)
   size_t len2 = strlen(s2);
   size_t n = 0;
 
-  if (len1 == 0 || len2 == 0)
-  {
+  if (len1 == 0 || len2 == 0) {
     return -1;
   }
 
-  while (len1 > 0 && len2 > 0)
-  {
-    if (s1[len1 - 1] != s2[len2 - 1])
-    {
+  while (len1 > 0 && len2 > 0) {
+    if (s1[len1 - 1] != s2[len2 - 1]) {
       break;
     }
-    ++n;
-    --len1;
-    --len2;
+    ++n; --len1; --len2;
+  }
+  return n;
+}
+
+/* like strlastcmp() but ignoring case */
+size_t strlastcasecmp(const char *s1, const char *s2)
+{
+  size_t len1 = strlen(s1);
+  size_t len2 = strlen(s2);
+  size_t n = 0;
+
+  if (len1 == 0 || len2 == 0) {
+    return -1;
   }
 
+  while (len1 > 0 && len2 > 0) {
+    if (tolower(s1[len1 - 1]) != tolower(s2[len2 - 1])) {
+      break;
+    }
+    ++n; --len1; --len2;
+  }
   return n;
 }
 
 /* Returns 1 if string s ends on string suf, otherwise 0 is returned. */
-int str_ends_on(const char *s, const char *suf)
-{
-  if (strlastcmp(s, suf) == strlen(suf))
-  {
+int str_ends_on(const char *s, const char *suf) {
+  if (strlastcmp(s, suf) == strlen(suf)) {
+    return 1;
+  }
+  return 0;
+}
+
+/* like str_ends_on() but ignoring case */
+int strcase_ends_on(const char *s, const char *suf) {
+  if (strlastcasecmp(s, suf) == strlen(suf)) {
     return 1;
   }
   return 0;
@@ -49,7 +72,8 @@ int main(void)
     *str2 = "Bukarest",
     *str3 = "abcdef",
     *str4 = "SONIC.exe",
-    *suf = ".exe",
+    *suf1 = ".exe",
+    *suf2 = ".EXE",
     *yesno = "does NOT end";
   size_t n;
 
@@ -68,14 +92,24 @@ int main(void)
   printf("last common chars: %ld\n\n", n);
 
   /* Test 3 */
-  if (str_ends_on(str4, suf))
-  {
+  if (str_ends_on(str4, suf1)) {
     yesno = "ends";
   }
   printf("Test 3\n");
   printf("string: %s\n", str4);
-  printf("suffix: %s\n", suf);
-  printf("`%s' %s on `%s'\n", str4, yesno, suf);
+  printf("suffix: %s\n", suf1);
+  printf("`%s' %s on `%s'\n\n", str4, yesno, suf1);
+
+  /* Test 4 */
+  if (strcase_ends_on(str4, suf2)) {
+    yesno = "ends";
+  } else {
+    yesno = "does NOT end";
+  }
+  printf("Test 4\n");
+  printf("string: %s\n", str4);
+  printf("suffix: %s\n", suf2);
+  printf("`%s' %s on `%s' (ignoring case)\n\n", str4, yesno, suf2);
 
   return 0;
 }
