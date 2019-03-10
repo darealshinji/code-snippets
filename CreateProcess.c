afb98308baca1baea4ca7ce9580ee68845a1d326
darealshinji <djcj@gmx.de>
2019-03-10 17:51:04 +0100

    Update CreateProcess.c

--- CreateProcess.c
+++ CreateProcess.c
@@ -1,67 +1,144 @@
 // Start a process relative to the .exe's location.
 
-// gcc -m32 -Wall -O3 -mwindows CreateProcess.c -o a.exe -s
-// cl /Ox /GS /guard:cf CreateProcess.c /link /out:a.exe user32.lib
+// i686-w64-mingw32-gcc -Wall -O3 -mwindows -municode CreateProcess.c -s -static
+
+
+// relative path to the main binary to start
+#define APPEND_EXE "prog.exe"
+
+// if defined, the program will check if it's running on
+// a 64 bit system and then start the 64 bit version
+#define APPEND_EXE64 "prog64.exe"
+
+/* Compile test programs from this:
+#include <windows.h>
+int main(int argc, char *argv[]) {
+  MessageBox(0, GetCommandLine(), "Test", MB_OK);
+  return 0;
+}
+*/
+
+
+#ifndef UNICODE
+# define UNICODE
+#endif
+#ifndef _UNICODE
+# define _UNICODE
+#endif
+#ifndef APPEND_EXE
+# error  APPEND_EXE not defined
+#endif
+#define BUFSIZE 4096
+#define PATH_MAX 32767
 
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define APPEND_EXE "bin\\win32\\program.exe"
-#define APPEND_SIZE 32
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+static void show_error_message(int errCode, const wchar_t *str, const wchar_t *title)
 {
-  LPCTSTR title = "Error";
-  UINT type = MB_ICONERROR | MB_OK;
+  int rv;
+  wchar_t buf[BUFSIZE];
+  wchar_t err[BUFSIZE];
+
+  rv = FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS,
+                      NULL,
+                      errCode,
+                      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                      buf,
+                      BUFSIZE,
+                      NULL);
+
+  if (rv == 0) {
+    _snwprintf_s(err, BUFSIZE, _TRUNCATE, L"%s failed with error code %ld", str, errCode);
+  } else {
+    _snwprintf_s(err, BUFSIZE, _TRUNCATE, L"%s failed:\n%s", str, buf);
+  }
+
+  MessageBoxW(0, err, title, MB_ICONERROR|MB_OK);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLine, int nCmdShow)
+{
+  const wchar_t *title = L"Error: " APPEND_EXE;
+  wchar_t modulePath[PATH_MAX];
+  wchar_t command[PATH_MAX];
+
+  // get exe path
+  if (!GetModuleFileNameW(NULL, modulePath, PATH_MAX)) {
+    MessageBoxW(0, L"GetModuleFileName() failed", title, MB_ICONERROR|MB_OK);
+    return 1;
+  }
+
+  // get dirname
+  wchar_t *p = wcsrchr(modulePath, L'\\');
+  if (!p) {
+    MessageBoxW(0, L"Cannot get executable path!", title, MB_ICONERROR|MB_OK);
+    return 1;
+  }
+  *p = 0;
+
+#ifdef APPEND_EXE64
+  SYSTEM_INFO sysi;
+  SecureZeroMemory(&sysi, sizeof(sysi));
+  GetNativeSystemInfo(&sysi);
+
+  // check if we're on a 64 bit OS
+  if (sysi.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64) {
+    WIN32_FIND_DATA FindFileData;
+    HANDLE handle;
+
+    // check if the 64 bit exe exists
+    _snwprintf_s(command, PATH_MAX, _TRUNCATE, L"%s\\" APPEND_EXE64, modulePath);
+    handle = FindFirstFileW(command, &FindFileData);
+
+    if (handle == INVALID_HANDLE_VALUE) {
+      // not found, use the 32 bit exe
+      _snwprintf_s(command, PATH_MAX, _TRUNCATE, L"%s\\" APPEND_EXE, modulePath);
+    } else {
+      title = L"Error: " APPEND_EXE64;
+      FindClose(handle);
+    }
+  }
+#else
+  _snwprintf_s(command, PATH_MAX, _TRUNCATE, L"%s\\" APPEND_EXE, modulePath);
+#endif
+
+  // append arguments
+  _snwprintf_s(command, PATH_MAX, _TRUNCATE, L"%s %s", command, pCmdLine);
+
+  // create process
   STARTUPINFO si;
   PROCESS_INFORMATION pi;
-
-  char moduleName[MAX_PATH];
-  char moduleRootDrive[_MAX_DRIVE];
-  char moduleRootDir[_MAX_DIR];
-  char command[_MAX_DRIVE + _MAX_DIR + APPEND_SIZE];
-  char error[64];
-  int rv = 0;
-
-  if (!GetModuleFileName(hInstance, moduleName, MAX_PATH)) {
-    MessageBox(0, "Failed calling GetModuleFileName()", title, type);
-    return 1;
-  }
-
-  if (_splitpath_s(moduleName, moduleRootDrive, _MAX_DRIVE, moduleRootDir, _MAX_DIR, NULL, 0, NULL, 0) != 0) {
-    MessageBox(0, "Failed calling _splitpath_s()", title, type);
-    return 1;
-  }
-
-  snprintf(command, _MAX_DRIVE + _MAX_DIR + APPEND_SIZE - 1, "%s%s" APPEND_EXE, moduleRootDrive, moduleRootDir);
 
   SecureZeroMemory(&si, sizeof(si));
   si.cb = sizeof(si);
   SecureZeroMemory(&pi, sizeof(pi));
 
-  if (!CreateProcess(NULL, command, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
-    snprintf(error, 63, "CreateProcess() failed (%ld).", (long)GetLastError());
-    MessageBox(0, error, title, type);
+  if (CreateProcessW(NULL, command, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi) == FALSE) {
+    int errCode = GetLastError();
+    show_error_message(errCode, L"CreateProcess()", title);
     return 1;
   }
 
-  rv = WaitForSingleObject(pi.hProcess, INFINITE);
+  int ret = WaitForSingleObject(pi.hProcess, INFINITE);
   CloseHandle(pi.hProcess);
   CloseHandle(pi.hThread);
 
-  if (rv == WAIT_ABANDONED) {
-    MessageBox(0, "Process abandoned.", title, type);
-    rv = 1;
-  } else if (rv == WAIT_TIMEOUT) {
-    MessageBox(0, "Process time-out error.", title, type);
-    rv = 1;
-  } else if (rv == WAIT_FAILED) {
-    snprintf(error, 63, "Process failed (%ld).", (long)GetLastError());
-    MessageBox(0, error, title, type);
-    rv = 1;
+  switch(ret) {
+    case WAIT_ABANDONED:
+      MessageBoxW(0, L"Process abandoned.", title, MB_ICONERROR|MB_OK);
+      return 1;
+    case WAIT_TIMEOUT:
+      MessageBoxW(0, L"Process time-out error.", title, MB_ICONERROR|MB_OK);
+      return 1;
+    case WAIT_FAILED:
+      int errCode = GetLastError();
+      show_error_message(errCode, L"Process", title);
+      return 1;
   }
 
-  return rv;
+  return 0;  // ret == WAIT_OBJECT_0
 }
 
