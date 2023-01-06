16d1e02af450695015488eb5c8ebaed61f8da31a
darealshinji <djcj@gmx.de>
2023-01-06 14:46:54 +0100

    Update ass-extract.cpp

--- ass-extract.cpp
+++ ass-extract.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#ifdef _WIN32
+#include <direct.h>
+#else
+#include <sys/stat.h>
+#endif
 
 
 static std::string src;
@@ -49,15 +54,26 @@ void ass_uudecode(const std::string &data, FILE *fp, bool flush)
 
 inline void ass_uudecode_flush(FILE *fp) {
   ass_uudecode("", fp, true);
-  fclose(fp);
+  if (fp) fclose(fp);
 }
 
-bool ass_extract(const std::string &file)
+/**
+ * file: .ass input
+ * dir: attachment output directory; empty == current dir
+ * extract: whether to extract attachments
+ * stripped: stripped .ass output; empty == disabled
+ */
+bool ass_extract(const std::string &file, std::string dir, bool extract, const std::string &stripped)
 {
   std::string line;
   std::fstream ifs, ofs;
   FILE *fp = NULL;
   size_t len;
+
+  if (!extract) {
+    if (stripped.empty()) return true; /* nothing to do */
+    dir.clear();
+  }
 
   ifs.open(file.c_str(), std::fstream::in);
 
@@ -65,6 +81,16 @@ bool ass_extract(const std::string &file)
     std::cerr << "error: cannot open file for reading: "
       << file << std::endl;
     return false;
+  }
+
+  if (!stripped.empty() && stripped != file) {
+    ofs.open(stripped.c_str(), std::fstream::out|std::fstream::binary);
+
+    if (!ofs.is_open()) {
+      std::cerr << "error: cannot open file for writing: "
+        << stripped << std::endl;
+      return false;
+    }
   }
 
   /* check first line */
@@ -85,18 +111,44 @@ bool ass_extract(const std::string &file)
     return false;
   }
 
+  if (ofs.is_open()) ofs << line << "\r\n";
+
   /* seek for [Fonts] or [Graphics] group */
   while (std::getline(ifs, line)) {
     if (line.back() == '\r') line.pop_back();
-    if (line == "[Events]") return true;
+    if (ofs.is_open()) ofs << line << "\r\n";
+    if (line == "[Events]") break;
     if (line == "[Fonts]" || line == "[Graphics]") goto JMP_DEC;
     if (line.empty()) continue;
   }
 
-  /* didn't find [Fonts], [Graphics] or [Events] */
-  return false;
+  /* didn't find [Fonts] or [Graphics] */
+  if (ofs.is_open()) {
+    ofs << line << "\r\n";
+
+    while (std::getline(ifs, line)) {
+      if (line.back() == '\r') line.pop_back();
+      ofs << line << "\r\n";
+    }
+
+    return true;
+  } else {
+    return (line == "[Events]") ? true : false;
+  }
 
 JMP_DEC:
+
+#ifdef _WIN32
+  if (!dir.empty()) {
+    _mkdir(dir.c_str());
+    if (dir.back() != '/' && dir.back() != '\\') dir += '\\';
+  }
+#else
+  if (!dir.empty() && dir != "/") {
+    mkdir(dir.c_str(), 00775);
+    if (dir.back() != '/') dir += '/';
+  }
+#endif
 
   /* decode files */
   while (std::getline(ifs, line)) {
@@ -130,7 +182,9 @@ JMP_DEC:
         line.erase(len-6, 2);
       }
 
-      if ((fp = fopen(line.c_str(), "wb")) == NULL)
+      line.insert(0, dir);
+
+      if (extract && (fp = fopen(line.c_str(), "wb")) == NULL)
       {
         std::cerr << "error: cannot open output file: "
           << line << std::endl;
@@ -144,14 +198,21 @@ JMP_DEC:
     ass_uudecode(line, fp, false);
   }
 
-  if (fp) ass_uudecode_flush(fp);
+  ass_uudecode_flush(fp);
+
+  if (ofs.is_open()) {
+    while (std::getline(ifs, line)) {
+      if (line.back() == '\r') line.pop_back();
+      ofs << line << "\r\n";
+    }
+  }
 
   return true;
 }
 
 int main()
 {
-  if (ass_extract("test.ass") == true) {
+  if (ass_extract("test.ass", "out", true, "test-stripped.ass") == true) {
     return 0;
   }
   return 1;
